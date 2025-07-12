56bf9e9986c8e3a0f17b261d4015e1cbd80d16cd
revatisankpal <revatisankpal6304@gmail.com~>
2025-07-12 16:02:08 +0530

    update

--- demo.js
+++ demo.js
@@ -1,9 +1,9 @@
-console.log("Started")
-console.log("End")
+console.log("Started");
 let a = 12;
 let b= 15;
 console.log("sum : ",a+b);
 console.log("Sub :",a-b);
 console.log("div :",a/b);
 console.log("Mul :",a*b);
+console.log("End");
 
